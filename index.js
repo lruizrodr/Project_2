8dfb0a510108ebabaf05eeb2a49ccbac36dd7c92
lruizrodr <lruiz0002@gmail.com>
2025-09-30 13:18:40 -0500

    styles, added btns, defined templates for cards

---
+++ index.js
@@ -0,0 +1,15 @@
+// API key: cugj8deZM3MxmsvUCaTVDxHMCxFQ10jB
+// Base URL: https://api.giphy.com/v1/gifs/
+// Endpoint: https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY&q=funny+cats&limit=10&offset=0&rating=g&lang=en
+
+
+
+
+// Will need to add event listener to the search bar to capture input and make API call based on that input. DONT FORGET
+// search //
+// https://api.giphy.com/v1/gifs/search?api_key=YOUR_API_KEY&q=funny+cats&limit=10&offset=0&rating=g&lang=en
+// random //
+//https://api.giphy.com/v1/gifs/random?api_key=YOUR_API_KEY&tag=funny&rating=g
+// trending //
+//https://api.giphy.com/v1/gifs/trending?api_key=YOUR_API_KEY&limit=10&rating=g
+// Will make a simple search first, if time allows, we can implement the other features.
